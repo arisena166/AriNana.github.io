3babe894497634640cdede5e8cc31e58ef098c03
arisena166 <arisena166@gmail.com>
2022-04-11 08:52:40 +0700

    baru

---
+++ script.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+ 
+  	if(a== "Nana" && b=="Nana"){
+  	    window.location.href="front.html";
+		
+  	} 
+	else{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
